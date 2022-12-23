7549f670a1f0bda9486644c111df9891ab9aa7f0
woldebrown <woldebrown009@yahoo.com>
2022-12-23 19:13:59 +0000

    Saved progress on CombineStrings 2022-12-23 19:13:59 +0000

--- CombineStrings/src/main/java/CombineStrings.java
+++ CombineStrings/src/main/java/CombineStrings.java
@@ -8,6 +8,6 @@ public class CombineStrings {
      * @return the combination of strings a and b.
      */
     public String combine(String a, String b){
-        return "";
+        return a + b;
     }
 }
