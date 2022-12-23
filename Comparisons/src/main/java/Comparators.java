2bb7114c8b01d1f268a5ba64e1079f2b9ff31d95
woldebrown <woldebrown009@yahoo.com>
2022-12-23 19:28:32 +0000

    Saved progress on Comparisons 2022-12-23 19:28:32 +0000

--- Comparisons/src/main/java/Comparators.java
+++ Comparisons/src/main/java/Comparators.java
@@ -14,6 +14,9 @@ public class Comparators {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
+        if(a == b) {
+            return true;
+        }
         return false;
     }
 
@@ -23,6 +26,9 @@ public class Comparators {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
+        if (a > b) {
+            return true;
+        }
         return false;
     }
     /**
@@ -31,6 +37,9 @@ public class Comparators {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
+        if(a < b) {
+            return true;
+        }
         return false;
     }
     /**
@@ -39,6 +48,9 @@ public class Comparators {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
+        if(a >= b) {
+            return true;
+        }
         return false;
     }
     /**
@@ -47,6 +59,9 @@ public class Comparators {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
+        if(a <= b) { 
+            return true;
+        }
         return false;
     }
 
@@ -56,6 +71,9 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
+        if(a != b) {
+            return true;
+        }
         return false;
     }
 }
