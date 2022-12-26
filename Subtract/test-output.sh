db83eea282416f55f8168795af65111a973e47e9
woldebrown <woldebrown009@yahoo.com>
2022-12-26 00:57:25 +0000

    Saved progress on Subtract 2022-12-26 00:57:25 +0000

---
+++ Subtract/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running SubtractTest
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.043 sec
+
+Results :
+
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0
+
