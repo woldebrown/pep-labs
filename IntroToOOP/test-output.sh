84bf54416bd1a2349ab212faf774ccafb44c18ac
woldebrown <woldebrown009@yahoo.com>
2022-12-26 01:28:33 +0000

    Saved progress on IntroToOOP 2022-12-26 01:28:33 +0000

---
+++ IntroToOOP/test-output.sh
@@ -0,0 +1,12 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running BirdTest
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.051 sec
+
+Results :
+
+Tests run: 3, Failures: 0, Errors: 0, Skipped: 0
+
