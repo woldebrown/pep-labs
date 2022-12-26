cc81893ba59ea305138679266c4759fb3329c76c
woldebrown <woldebrown009@yahoo.com>
2022-12-26 01:12:30 +0000

    Saved progress on Start 2022-12-26 01:12:30 +0000

---
+++ Start/test-output.sh
@@ -0,0 +1,16 @@
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
+Running GuideTest
+Expected: Success from guide object 1! it says: hello
+Actual: Success from guide object 1! it says: hello
+Expected: Success from guide object 1! it says: test
+Actual: Success from guide object 1! it says: test
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.094 sec
+
+Results :
+
+Tests run: 2, Failures: 0, Errors: 0, Skipped: 0
+
