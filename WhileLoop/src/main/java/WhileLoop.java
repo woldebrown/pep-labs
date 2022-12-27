6e5c037aa828c0daff54473b72f2a3f420da9b48
woldebrown <woldebrown009@yahoo.com>
2022-12-27 21:01:58 +0000

    Saved progress on WhileLoop 2022-12-27 21:01:58 +0000

--- WhileLoop/src/main/java/WhileLoop.java
+++ WhileLoop/src/main/java/WhileLoop.java
@@ -36,8 +36,10 @@ public class WhileLoop {
         int sum = 0;
         int i = 0;
 
-//        code here
-
+       while(i < end) {
+            sum = sum + i;
+            i=+ 1;
+        }
         return sum;
     }
 }
