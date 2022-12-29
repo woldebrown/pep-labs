e51a70f36fe20a689d4e14a5311bfb8e721fcb6d
woldebrown <woldebrown009@yahoo.com>
2022-12-29 09:31:30 +0000

    Saved progress on SetAllArrayElementsToN 2022-12-29 09:31:30 +0000

--- SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
+++ SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
@@ -23,6 +23,9 @@ public class SetAllArrayElements {
      * @return nothing, because of pass-by-reference, all changes will be reflected in the original array.
      */
     public void set(int[] arr, int n){
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = n;
+        }
 
     }
 }
