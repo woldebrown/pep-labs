38b4f967fc95e59e355993d21932bb76dc287000
woldebrown <woldebrown009@yahoo.com>
2022-12-29 09:25:13 +0000

    Saved progress on SetAllArrayElementsToN 2022-12-29 09:25:13 +0000

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -15,15 +15,14 @@ public class FizzBuzz {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
-        if(n % 3 == 0) {
-            return "Fizz";
-        } else if(n % 5 ==0) {
+        if(n % 5 == 0) {
             return "Buzz";
-        } else if(n % 3 == 0 && n % 5 == 0) {
+        } if(n % 3 ==0) {
+            return "Fizz";
+        } else if(n % 3 ==0 && n % 5 == 0) {
             return "FizzBuzz";
-        } else {
-            return "";
-
-        }
+        } 
+        return "";
+    
     }
 }
