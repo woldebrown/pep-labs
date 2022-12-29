a094eb8ebbb4c0ab4bb1a9f958969a176b61889f
woldebrown <woldebrown009@yahoo.com>
2022-12-29 21:03:35 +0000

    Saved progress on FizzBuzz 2022-12-29 21:03:35 +0000

--- ReturnArrayContents/src/main/java/ReturnArrayContents.java
+++ ReturnArrayContents/src/main/java/ReturnArrayContents.java
@@ -28,6 +28,10 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String[] str = nums.length;
+        for(int i = 0; i < nums.length; i++) {
+        }
+        return nums[i] + " ";
+
     }
 }
