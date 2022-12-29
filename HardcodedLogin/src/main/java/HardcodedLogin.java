eee8f80e4b9314023413ba8d1368e56d3ab8480e
woldebrown <woldebrown009@yahoo.com>
2022-12-29 04:10:09 +0000

    Saved progress on HardcodedLogin 2022-12-29 04:10:09 +0000

--- HardcodedLogin/src/main/java/HardcodedLogin.java
+++ HardcodedLogin/src/main/java/HardcodedLogin.java
@@ -15,6 +15,9 @@ public class HardcodedLogin {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
+        if(username == "admin" && password == "qwerty" || username == "user" && password == "password"){
+            return true;
+        }
         return false;
     }
 }
