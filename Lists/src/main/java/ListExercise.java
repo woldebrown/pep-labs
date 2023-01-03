d388ad47916e67aaadede902825c2dd778c5c2a9
woldebrown <woldebrown009@yahoo.com>
2023-01-03 21:54:32 +0000

    Saved progress on Exceptions 2023-01-03 21:54:32 +0000

--- Lists/src/main/java/ListExercise.java
+++ Lists/src/main/java/ListExercise.java
@@ -30,7 +30,8 @@ public class ListExercise {
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        return null;
+        List<Integer> list = new ArrayList<>();
+        return list;
     }
 
     /**
@@ -40,7 +41,7 @@ public class ListExercise {
      * @return the size of List (number of items it holds.)
      */
     public int getSize(List<Integer> list){
-        return 0;
+        return list.size();
     }
 
     /**
@@ -52,6 +53,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
+        list.add(value);
     }
 
     /**
@@ -67,7 +69,7 @@ public class ListExercise {
      * @return the int at the location in 'list' represented by 'index'.
      */
     public int get(List<Integer> list, int index){
-        return 0;
+        return list.get(index);
     }
 
     /**
@@ -80,6 +82,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeFromList(List<Integer> list, int position){
+        list.remove(position);
 
     }
 
@@ -93,5 +96,6 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void updateAtPosition(List<Integer> list, int position, int value){
+        list.add(position, value);
     }
 }
