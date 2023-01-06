88f059c2c627ffa8a188f535045501f645eec7aa
woldebrown <woldebrown009@yahoo.com>
2023-01-06 08:02:36 +0000

    Saved progress on GetMax 2023-01-06 08:02:36 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -18,6 +18,7 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
+        
         return 0;
     }
 }
--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i ++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
     }
 }
