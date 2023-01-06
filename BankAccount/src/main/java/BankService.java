c3059458df7b8cbda9a8ff1b88bb002071f34d37
woldebrown <woldebrown009@yahoo.com>
2023-01-06 18:31:02 +0000

    Saved progress on BankAccount 2023-01-06 18:31:02 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,6 +23,10 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
+        if(amount > 0) {
+            this.balance = balance + amount;
+        }
+        
 
     }
 
@@ -32,7 +36,11 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if(balance < amount) {
+            System.out.println("");
+        } else {
+        this.balance = balance - amount;
+        }
     }
 
     /**
@@ -40,6 +48,7 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+
+        return balance;
     }
 }
