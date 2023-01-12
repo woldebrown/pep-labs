2f90c72f0129b41b01e128c9d839a5252859731d
woldebrown <woldebrown009@yahoo.com>
2023-01-12 07:35:49 +0000

    Saved progress on DoesNotContain 2023-01-12 07:35:49 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -13,6 +13,11 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
-        return false;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == target) {
+                return false;
+            }
+        }
+        return true;
     }
 }
