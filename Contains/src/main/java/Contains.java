c817354c1b4f737a744921f441e80705415a9e02
woldebrown <woldebrown009@yahoo.com>
2023-01-12 07:26:07 +0000

    Saved progress on Contains 2023-01-12 07:26:07 +0000

--- Contains/src/main/java/Contains.java
+++ Contains/src/main/java/Contains.java
@@ -11,6 +11,11 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == target) {
+                return true;
+            }
+        }
         return false;
     }
 }
