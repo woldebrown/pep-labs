c75d54c8dd85fa1adc3a8af8d9c31c8a3a742091
woldebrown <woldebrown009@yahoo.com>
2023-01-18 00:55:27 +0000

    Saved progress on FizzBuzz 2023-01-18 00:55:27 +0000

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -17,9 +17,9 @@ public class FizzBuzz {
     public String FizzBuzzGen(int n){
         if(n % 3 == 0 && n % 5 == 0) {
             return "FizzBuzz";
-        } else if( n % 3 == 0) {
+        } if( n % 3 == 0) {
             return "Fizz";
-        } else if(n % 5 ==0) {
+        } if(n % 5 ==0) {
             return "Buzz";
         } else {
             return "";
