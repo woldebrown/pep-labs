7ae9d1861603b4a89759c5691e7c521ab8c22509
woldebrown <woldebrown009@yahoo.com>
2023-01-19 07:58:28 +0000

    Saved progress on CreatingEndpoints 2023-01-19 07:58:28 +0000

--- PathParameters/src/main/java/com/revature/JavalinSingleton.java
+++ PathParameters/src/main/java/com/revature/JavalinSingleton.java
@@ -16,7 +16,7 @@ public class JavalinSingleton {
          */
         app.get("/firstname/{first}", ctx -> {
             
-            //write code here
+            String firstname = ctx.pathParam("first");
 
         });
 
