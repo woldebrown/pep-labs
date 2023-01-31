5208b7f6ca9163e1caf91d70b7bbe3b6e3f6a48c
woldebrown <woldebrown009@yahoo.com>
2023-01-31 21:47:48 +0000

    Saved progress on ForeignKey 2023-01-31 21:47:48 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,5 +1,1 @@
-CREATE TABLE post (
-    id SERIAL PRIMARY KEY,
-    post varchar(255),
-    user_fk int REFERENCES user(id)
-);
+CREATE TABLE post (id SERIAL PRIMARY KEY, post varchar(255), user_fk int REFERENCES site_user(id));
